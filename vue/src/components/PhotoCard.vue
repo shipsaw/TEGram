b53901d579f3bfd90bd5e6c2fd9cee49c9134906
Kacie Greenlief <kaciecraig.21@gmail.com>
2021-08-05 15:30:52 -0400

    some frontend stuff

--- vue/src/components/PhotoCard.vue
+++ vue/src/components/PhotoCard.vue
@@ -3,35 +3,37 @@
 
 <div class="card" > 
 
-<!-- //grab sample of pics and v-for through to render -->
-<!-- 
-<div class="picCard" v-for="pic in picFeed" v-bind:key="pic.id">
+
+<div class="picCard" v-for="pic in " v-bind:key="pic.id">
 {{pic.url}}
 {{pic.comments}}
  <i class="fa fa-dribbble fa-4x"></i>
-</div> -->
+</div>
 
 
-
-
-    <img src="@/images/logo.png" alt="">
-    <img src="@/images/cat0.png" alt="">
-    <img src="@/images/cutekitten.png" alt="">
-      <img src="@/images/cat0.png" alt="">
-    <img src="@/images/cutekitten.png" alt="">
-      <img src="@/images/cat0.png" alt="">
-    <img src="@/images/cutekitten.png" alt="">
-    <div>caption goes here-ish</div> 
-    
 </div>
   <!-- in feed: pic, who posted it, first two comments
   like icon, add to favorites icon -->
 </template>
 
 <script>
+
+import photoService from '@/services/PhotoService.js'
+
 export default {
  name: 'pics-list',
-  props: ['pics']
+  props: ['pics'],
+
+
+methods:{
+  retrieveFeed(){
+     photoService.getPhotos().then(response=>{
+       
+     })
+  }
+
+
+  }
 }
 </script>
 
