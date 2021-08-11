088f8ef3292f85c99464040cbf237157ee9ce8a7
Stephen Mick <stephen.mick@gmail.com>
2021-08-11 11:25:02 -0400

    added nav bar router links

--- vue/src/components/NavBar.vue
+++ vue/src/components/NavBar.vue
@@ -3,10 +3,12 @@
     <nav class="nav-div">
       <router-link v-bind:to="{ name: 'home' }" >  <img src="@/images/logo.png" class="logo-pic" />   </router-link>&nbsp;&nbsp;
       <!-- user's profile photo -->
-      <router-link v-bind:to="{name: 'home' }"> <img v-bind:src="$store.state.user.userProfileUrl" class="profile-photo" /> </router-link>
+      <router-link v-bind:to="{name: 'friends-gallery', params:{data: $store.state.user.userId} }"> <img v-bind:src="$store.state.user.userProfileUrl" class="profile-photo" /> </router-link>
       <b-container  >
         <b-dropdown  class="dropdown ">
           <b-dropdown-item id="button-style">
+                      <b-dropdown-item>  <router-link v-bind:to="{name: 'home'}">Home</router-link></b-dropdown-item>
+
             <router-link v-bind:to="{name: 'friends-gallery', params:{data: $store.state.user.userId}}">My Gallery</router-link>
                   </b-dropdown-item>
           <b-dropdown-item>  <router-link v-bind:to="{name: 'user-faves'}">My Favorites</router-link></b-dropdown-item>
